8aad3222433f76f9287c57b15c1ea4c981a37aec
Diego Suntaxi <diego.suntaxi@outlook.es>
2023-10-08 23:12:08 -0500

    feat(commits): Commit Item component

---
+++ src/components/CommitItem.tsx
@@ -0,0 +1,37 @@
+import { CommitElement } from "../api/commits.api";
+
+interface CommitItemProps {
+  commit: CommitElement;
+}
+
+const formatDate = (dateString: Date) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
+
+function CommitItem({ commit }: CommitItemProps) {
+  return (
+    <li
+      key={commit.sha}
+      className="group mb-4 border-b pb-4 last:border-0 hover:bg-purple-100"
+    >
+      <a
+        href={commit.html_url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-indigo-600 hover:underline block mb-2 group-hover:text-purple-400"
+      >
+        {commit.commit.message}
+      </a>
+      <span className="text-gray-600">
+        {commit.commit.author.name} • {formatDate(commit.commit.author.date)}
+      </span>
+    </li>
+  );
+}
+
+export default CommitItem;
--- src/components/CommitList.tsx
+++ src/components/CommitList.tsx
@@ -1,39 +1,15 @@
 import { CommitElement } from "../api/commits.api";
+import CommitItem from "./CommitItem";
 
 interface CommitListProps {
   commits: CommitElement[];
 }
 
 function CommitList({ commits }: CommitListProps) {
-  const formatDate = (dateString: Date) => {
-    const options: Intl.DateTimeFormatOptions = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-
   return (
     <ul>
       {commits.map((commit) => (
-        <li
-          key={commit.sha}
-          className="group mb-4 border-b pb-4 last:border-0 hover:bg-purple-100"
-        >
-          <a
-            href={commit.html_url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-indigo-600 hover:underline block mb-2 group-hover:text-purple-400"
-          >
-            {commit.commit.message}
-          </a>
-          <span className="text-gray-600">
-            {commit.commit.author.name} •{" "}
-            {formatDate(commit.commit.author.date)}
-          </span>
-        </li>
+        <CommitItem key={commit.sha} commit={commit} />
       ))}
     </ul>
   );
